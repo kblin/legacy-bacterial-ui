10a19e6c991339086ca9031b027204f0dc1d514a
Kai Blin <kblin@biosustain.dtu.dk>
2023-02-24 00:35:47 +0100

    experimentalsmash changes
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- app/start/start.js
+++ app/start/start.js
@@ -12,42 +12,19 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
 
             vm.upload_sideload_file = false;
 
+            vm.jobtype_options = [
+                { id: "", name: "Please select a jobtype" },
+                { id: "paras", name: "PARAS" },
+                { id: "test", name: "test" },
+            ];
+            vm.selected_jobtype = { id: "", name: "Please select a jobtype" };
+
             // Defaullt values
             vm.submission = {};
 
-            vm.extra_features = [
-                { id: 'knownclusterblast', description: 'KnownClusterBlast', default: true, stable: true, beta: true },
-                { id: 'clusterblast', description: 'ClusterBlast', default: false, stable: true, beta: true },
-                { id: 'subclusterblast', description: 'SubClusterBlast', default: true, stable: true, beta: true },
-                { id: 'cc_mibig', description: 'MIBiG cluster comparison', default: false, stable: true, beta: true },
-                { id: 'asf', description: 'ActiveSiteFinder', default: true, stable: true, beta: true },
-                { id: 'rre', description: 'RREFinder', default: true, stable: true, beta: true },
-                { id: 'clusterhmmer', description: 'Cluster Pfam analysis', default: false, stable: true, beta: true },
-                { id: 'pfam2go', description: 'Pfam-based GO term annotation', default: false, stable: true, beta: true },
-                { id: 'tigrfam', description: 'TIGRFam analysis', default: false, stable: true, beta: true },
-                { id: 'tfbs', description: 'TFBS analysis', default: true, stable: false, beta: true },
-            ];
-
-            for (var i = 0; i < vm.extra_features.length; i++) {
-                var feature = vm.extra_features[i];
-                vm.submission[feature.id] = feature.default;
-            }
-
-            vm.strictness_levels = [
-                { id: 'strict', description: 'Detects well-defined clusters containing all required parts.' },
-                { id: 'relaxed', description: 'Detects partial clusters missing one or more functional parts.' },
-                {
-                    id: 'loose', description: 'Detects poorly-defined clusters and clusters that likely match primary metabolites.',
-                    warning: 'Likely to cause false positives.'
-                },
-            ];
-
-            vm.hmmdetection_strictness = 1;
-
-            vm.genefinder = 'prodigal';
 
             vm.submit = function (form) {
-                vm.submission.jobtype = vm.run_beta ? 'antismash7' : 'antismash6';
+                vm.submission.jobtype = `experimentalsmash-${vm.selected_jobtype.id}`;
                 vm.active_submission = true;
                 vm.errror_message = null;
 
@@ -63,25 +40,9 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
                     vm.submission.ncbi = vm.ncbi;
                 }
 
-                if (vm.sideload_file) {
-                    vm.submission.sideload = vm.sideload_file;
-                }
-
                 if (vm.email) {
                     vm.submission.email = vm.email;
                 }
-
-                for (var i = 0; i < vm.extra_features.length; i++) {
-                    var feature = vm.extra_features[i];
-                    if (vm.run_beta && !feature.beta) {
-                        delete vm.submission[feature.id];
-                    }
-                    if (!vm.run_beta && !feature.stable) {
-                        delete vm.submission[feature.id];
-                    }
-                }
-
-                vm.submission.hmmdetection_strictness = vm.strictness_levels[vm.hmmdetection_strictness].id;
 
                 Upload.upload({
                     url: '/api/v1.0/submit',
@@ -157,22 +118,12 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
                         return false;
                     }
                 }
+                if (vm.selected_jobtype.id === "") {
+                    return false;
+                }
                 return true;
             }
 
-            vm.allOff = function () {
-                for (var i = 0; i < vm.extra_features.length; i++) {
-                    var feature = vm.extra_features[i];
-                    vm.submission[feature.id] = false;
-                }
-            }
-
-            vm.allOn = function () {
-                for (var i = 0; i < vm.extra_features.length; i++) {
-                    var feature = vm.extra_features[i];
-                    vm.submission[feature.id] = true;
-                }
-            }
 
             vm.loadSampleInput = function () {
                 vm.upload_file = false;
@@ -201,21 +152,4 @@ angular.module('antismash.ui.bacterial.as_start', ['ngFileUpload'])
                 vm.gff_file = null;
             }
 
-            vm.strictness_descriptions = function () {
-                var descriptions = [];
-                for (var i = 0; i <= vm.hmmdetection_strictness; i++) {
-                    descriptions.push(vm.strictness_levels[i].description);
-                }
-                return descriptions;
-            }
-
-            vm.filterFeatureByStatus = function (value) {
-                if (value.beta && vm.run_beta) {
-                    return true;
-                }
-                if (value.stable && !vm.run_beta) {
-                    return true;
-                }
-                return false;
-            }
         }]);
